fc09849128e74771e769adb60e29ffc988f0e04a
lucasezidro <lucasezidro7@gmail.com>
2024-06-24 09:07:14 -0300

    create api folder

---
+++ apps/api/src/index.ts
@@ -0,0 +1,7 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUser = ability.can('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUser)
