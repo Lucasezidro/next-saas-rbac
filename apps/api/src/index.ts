7c13e02f955c183ad36b01c2b18db964556213db
lucasezidro <lucasezidro7@gmail.com>
2024-06-24 22:03:50 -0300

    create first test route for create users and create prisma schema with all tablees

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const userCanDeleteOtherUser = ability.can('delete', 'User')
-
-console.log(userCanDeleteOtherUser)
