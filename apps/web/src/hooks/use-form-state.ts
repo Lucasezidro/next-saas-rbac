4bcfd8ccf975e03a8af1916fce35e48e583d2e88
lucasezidro <lucasezidro7@gmail.com>
2024-06-30 10:38:51 -0300

    create flow for auth user

--- apps/web/src/hooks/use-form-state.ts
+++ apps/web/src/hooks/use-form-state.ts
@@ -8,6 +8,7 @@ interface FormState {
 
 export function useFormState(
   action: (data: FormData) => Promise<FormState>,
+  onSuccess: () => Promise<void> | void,
   initialState?: FormState,
 ) {
   const [isPending, startTransition] = useTransition()
@@ -29,6 +30,10 @@ export function useFormState(
 
     startTransition(async () => {
       const result = await action(data)
+
+      if (result.success === true && onSuccess) {
+        await onSuccess()
+      }
 
       setFormState(result)
     })
