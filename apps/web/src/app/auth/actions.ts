11ad0eb62a909f2951467abae0b00d44f6f80ac0
lucasezidro <lucasezidro7@gmail.com>
2024-07-01 11:33:50 -0300

    use environment variables in frontend

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,15 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
   const githubSignUrl = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignUrl.searchParams.set('client_id', 'Ov23lidAO7UAlpyCfgQX')
+  githubSignUrl.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignUrl.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   githubSignUrl.searchParams.set('scope', 'user')
 
