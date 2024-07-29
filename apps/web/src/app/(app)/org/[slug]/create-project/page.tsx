b3acd6193b709f4bc589d39e69de6e3828b4138d
lucasezidro <lucasezidro7@gmail.com>
2024-07-29 09:16:56 -0300

    create tabs and current active link

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,16 +1,21 @@
-import { Header } from '@/components/header'
+import { redirect } from 'next/navigation'
+
+import { ability } from '@/auth/auth'
 
 import { ProjectForm } from './project-form'
 
-export default function CreateProject() {
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
+export default async function CreateProject() {
+  const permissions = await ability()
 
-        <ProjectForm />
-      </main>
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
+
+      <ProjectForm />
     </div>
   )
 }
